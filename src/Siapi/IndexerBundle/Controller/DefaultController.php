39b385e73db5c644feb106e84ab6e3be5d19f7b7
Mickael Vieira <contact@mickael-vieira.com>
2015-03-30 21:12:00 +0100

    Fixed bundle name in controller

--- src/Siapi/IndexerBundle/Controller/DefaultController.php
+++ src/Siapi/IndexerBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('SiapiDownloadBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('SiapiIndexerBundle:Default:index.html.twig', array('name' => $name));
     }
 }
